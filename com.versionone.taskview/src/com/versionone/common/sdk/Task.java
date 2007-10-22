c211d818ce0bc89214285e9b8e6f939b6e72936b
jerry <jerry@542498e7-56f8-f544-a0f2-a85913ac812a>
2007-10-22 02:34:39 +0000

    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/eclipse/addin@6629 542498e7-56f8-f544-a0f2-a85913ac812a

---
+++ com.versionone.taskview/src/com/versionone/common/sdk/Task.java
@@ -0,0 +1,54 @@
+package com.versionone.common.sdk;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.versionone.apiclient.APIException;
+import com.versionone.apiclient.Attribute;
+
+/**
+ * This class represents one Task in the VersionOne system
+ * @author jerry
+ */
+public class Task {
+
+	private static final String TASK_PREFIX = "Task.";
+	
+	HashMap<String, String> _values = new HashMap<String, String>();
+
+	/**
+	 * Create with uneditable items
+	 * @param token
+	 * @param id
+	 * @param storyName
+	 */
+	public Task(String token){
+		super();
+		_values.put("Token", token);
+	}
+	
+	public void setAttributeValues(Map<String, Attribute> value) throws APIException {
+		Iterator<String> iter = value.keySet().iterator();
+		while(iter.hasNext()) {
+			String key = iter.next();
+			Attribute attribute = value.get(key);
+			if( (null != attribute) && (null != attribute.getValue())){
+				if(key.startsWith(TASK_PREFIX)) {
+					key = key.substring(TASK_PREFIX.length());
+				}
+				_values.put(key, attribute.getValue().toString());
+			}
+//			else {
+//				Activator.logInfo("No value for " + key);
+//			}
+		}
+	}
+	
+	public String getValue(String key) {
+		if(_values.containsKey(key)) {
+			return _values.get(key);
+		}
+		return "";
+	}	
+}

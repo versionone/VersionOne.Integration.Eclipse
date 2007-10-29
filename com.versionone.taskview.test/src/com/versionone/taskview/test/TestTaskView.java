e3c455ff969357531c0f9dc1dfeea3a1131e6a4a
jerry <jerry@542498e7-56f8-f544-a0f2-a85913ac812a>
2007-10-29 21:08:39 +0000

    D-04064 - Column headers are localized
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/eclipse/addin@6883 542498e7-56f8-f544-a0f2-a85913ac812a

--- com.versionone.taskview.test/src/com/versionone/taskview/test/TestTaskView.java
+++ com.versionone.taskview.test/src/com/versionone/taskview/test/TestTaskView.java
@@ -14,6 +14,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.versionone.apiclient.FileAPIConnector;
+import com.versionone.apiclient.Localizer;
 import com.versionone.apiclient.MetaModel;
 import com.versionone.apiclient.Services;
 import com.versionone.common.preferences.PreferenceConstants;
@@ -57,9 +58,11 @@ public class TestTaskView {
 	public static void loadTestData() {
 		FileAPIConnector metaConnector = new FileAPIConnector("testdata/TestMetaData.xml", "meta.v1/");
 		FileAPIConnector dataConnector = new FileAPIConnector("testdata/TestData.xml", "rest-1.v1/");
+		FileAPIConnector localizeConnector = new FileAPIConnector("testdata/TestLocalizeData.xml", "loc.v1/");
 		MetaModel metaModel = new MetaModel(metaConnector);
-		Services services = new Services(metaModel, dataConnector);		
-		V1Server.initialize(services, metaModel);
+		Services services = new Services(metaModel, dataConnector);	
+		Localizer localizer = new Localizer(localizeConnector);
+		V1Server.initialize(services, metaModel, localizer);
 	}
 	
 	@Before

d971fe082f9791a7ebf2332f4179e5ce98e149a8
Eugene <Eugene@542498e7-56f8-f544-a0f2-a85913ac812a>
2009-08-14 15:42:19 +0000

    actions bound to context menu, must be buggy or even non-working
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/eclipse/addin@28644 542498e7-56f8-f544-a0f2-a85913ac812a

--- com.versionone.taskview/src/com/versionone/taskview/views/TaskView.java
+++ com.versionone.taskview/src/com/versionone/taskview/views/TaskView.java
@@ -123,31 +123,47 @@ public class TaskView extends ViewPart implements IPropertyChangeListener {
     	
     	final MenuItem closeItem = new MenuItem(menu, SWT.PUSH);
     	closeItem.setText(MENU_ITEM_CLOSE_KEY);
-        closeItem.addListener(SWT.Selection, new Listener() {
-            public void handleEvent(Event e) {
-                System.out.println(MENU_ITEM_CLOSE_KEY);
-            }
-        });
+    	closeItem.addListener(SWT.Selection, new Listener() {
+    		public void handleEvent(Event e) {
+    			CloseWorkitemDialog closeDialog = new CloseWorkitemDialog(control.getShell(), getCurrentWorkitem());
+    			closeDialog.setBlockOnOpen(true);
+    			closeDialog.open();
+    		}
+    	});
     	menuItemsMap.put(MENU_ITEM_CLOSE_KEY, closeItem);
     	
     	final MenuItem quickCloseItem = new MenuItem(menu, SWT.PUSH);
     	quickCloseItem.setText(MENU_ITEM_QUICK_CLOSE_KEY);
-        quickCloseItem.addListener(SWT.Selection, new Listener() {
-            public void handleEvent(Event e) {
-                System.out.println(MENU_ITEM_QUICK_CLOSE_KEY);
-            }
-        });
+    	quickCloseItem.addListener(SWT.Selection, new Listener() {
+    		public void handleEvent(Event e) {
+    			try {
+    				getCurrentWorkitem().quickClose();
+    				refreshTreeViewer();
+    			} catch(DataLayerException ex) {
+    				Activator.logError(ex);
+    				MessageDialog.openError(control.getShell(), "Task View Error",
+                          "Error during closing Workitem. Check Error Log for more details.");
+    			}
+    		}
+    	});
     	menuItemsMap.put(MENU_ITEM_QUICK_CLOSE_KEY, quickCloseItem);
     	
     	new MenuItem(menu, SWT.SEPARATOR);
     	
     	final MenuItem signupItem = new MenuItem(menu, SWT.PUSH);
     	signupItem.setText(MENU_ITEM_SIGNUP_KEY);
-        signupItem.addListener(SWT.Selection, new Listener() {
-            public void handleEvent(Event e) {
-                System.out.println(MENU_ITEM_SIGNUP_KEY);
-            }
-        });
+    	signupItem.addListener(SWT.Selection, new Listener() {
+    		public void handleEvent(Event e) {
+    			try {
+    				getCurrentWorkitem().signup();
+    				refreshTreeViewer();
+    			} catch(DataLayerException ex) {
+    				Activator.logError(ex);
+    				MessageDialog.openError(control.getShell(), "Task View Error",
+                          "Error during signing up. Check Error Log for more details.");
+    			}
+    		}
+    	});
     	menuItemsMap.put(MENU_ITEM_SIGNUP_KEY, signupItem);
     	
     	new MenuItem(menu, SWT.SEPARATOR);
@@ -155,33 +171,39 @@ public class TaskView extends ViewPart implements IPropertyChangeListener {
     	MenuItem editDescription = new MenuItem(menu, SWT.PUSH);
     	editDescription.setText(MENU_ITEM_EDIT_DESCRIPTION_KEY);
     	final TreeViewer tmpViewer = viewer;
-        editDescription.addListener(SWT.Selection, new Listener() {
-            public void handleEvent(Event e) {
-                HTMLEditor htmlEditor = new HTMLEditor(tmpViewer.getControl().getShell(), getCurrentWorkitem());
-                htmlEditor.create();
-                htmlEditor.open();
-            }
+    	editDescription.addListener(SWT.Selection, new Listener() {
+                public void handleEvent(Event e) {
+                    //HTMLEditor htmlEditor = new HTMLEditor(tmpViewer.getControl().getShell(), getCurrentWorkitem());
+                    //htmlEditor.create();
+                    //htmlEditor.open(); 
+                }
         });
         menuItemsMap.put(MENU_ITEM_EDIT_DESCRIPTION_KEY, editDescription);
     	
-        menu.addMenuListener(new MenuListener() {
+    	menu.addMenuListener(new MenuListener() {
 
-            public void menuHidden(MenuEvent e) {
-            }
+			public void menuHidden(MenuEvent e) { }
 
-            public void menuShown(MenuEvent e) {
-                Workitem item = getCurrentWorkitem();
-                if (menu.getVisible() && (item == null || !validRowSelected())) {
-                    menu.setVisible(false);
-                }
-
-                quickCloseItem.setEnabled(item.canQuickClose());
-                signupItem.setEnabled(item.canSignup());
-            }
-        });
+			public void menuShown(MenuEvent e) {
+				Workitem item = getCurrentWorkitem();
+				if(menu.getVisible() && (item == null || !validRowSelected())) {
+					menu.setVisible(false);
+				}
+				
+				quickCloseItem.setEnabled(item.canQuickClose());
+				signupItem.setEnabled(item.canSignup());
+			}
+    	});
     	control.setMenu(menu);
     }
 
+    /**
+     * Refresh viewer, causing it to re-read data from model and remove possibly non-relevant items.
+     */
+    private void refreshTreeViewer() {
+    	viewer.refresh();
+    }
+    
     /**
      * Select the content and label providers
      */

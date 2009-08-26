c944b22c4cc9df353889bf620753b153d513ebde
Denis <Denis@542498e7-56f8-f544-a0f2-a85913ac812a>
2009-08-26 12:42:21 +0000

    current value set to Owner editor
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/eclipse/addin@29025 542498e7-56f8-f544-a0f2-a85913ac812a

--- com.versionone.taskview/src/com/versionone/taskview/views/editors/MultiValueEditor.java
+++ com.versionone.taskview/src/com/versionone/taskview/views/editors/MultiValueEditor.java
@@ -1,6 +1,7 @@
 package com.versionone.taskview.views.editors;
 
 import org.eclipse.jface.viewers.DialogCellEditor;
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Tree;
@@ -23,7 +24,10 @@ public class MultiValueEditor extends DialogCellEditor {
         PropertyValues value = (PropertyValues) getValue();
         dialog.setSelectedIndices(getIndices(value));
         int x = dialog.open();
-        return dialog.getSelectedIndices();
+        if (x == Window.OK){
+            return dialog.getSelectedIndices();
+        }
+        return value;
     }
 
     private int[] getIndices(PropertyValues property) {

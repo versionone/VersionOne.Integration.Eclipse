bbe026b7c278239abb7ba6733a2656dbc447e8c6
Eugene <Eugene@542498e7-56f8-f544-a0f2-a85913ac812a>
2009-09-01 09:12:00 +0000

    some progress not to lose it
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/eclipse/addin@29241 542498e7-56f8-f544-a0f2-a85913ac812a

--- com.versionone.taskview/src/com/versionone/taskview/views/editors/SingleValueEditor.java
+++ com.versionone.taskview/src/com/versionone/taskview/views/editors/SingleValueEditor.java
@@ -5,14 +5,40 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
 import com.versionone.common.sdk.ApiDataLayer;
-import com.versionone.common.sdk.Workitem;
+import com.versionone.common.sdk.PropertyValues;
+import com.versionone.common.sdk.ValueId;
 
 public class SingleValueEditor extends ComboBoxCellEditor {
-	private ApiDataLayer dataLayer;
+	private final PropertyValues allValues;
 	
-	public SingleValueEditor(Composite parent, String[] items, Workitem workitem) {
+	public SingleValueEditor(Composite parent, String typePrefix, String propertyName) {
 		super(parent, null, SWT.DROP_DOWN | SWT.READ_ONLY);
-		dataLayer = ApiDataLayer.getInstance();
+		allValues = ApiDataLayer.getInstance().getListPropertyValues(typePrefix, propertyName);
+		setItems(allValues.toStringArray());
 	}
-
+	
+	@Override
+	protected void doSetValue(Object value) {
+		super.doSetValue(getIndex((PropertyValues)value));
+	}
+	
+	@Override
+	protected Object doGetValue() {
+		Integer index = (Integer)super.doGetValue();
+		return getPropertyValues(index);
+	}
+	
+	private PropertyValues getPropertyValues(int index) {
+        PropertyValues res = new PropertyValues();
+        res.add(allValues.getValueIdByIndex(index));
+        return res;
+    }
+	
+	private Integer getIndex(PropertyValues property) {
+        Integer index = -1;
+        for (ValueId id : property){
+            index = allValues.getStringArrayIndex(id);
+        }
+        return index;
+    }
 }
